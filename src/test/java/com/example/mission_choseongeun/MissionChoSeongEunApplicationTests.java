96feedd432e55f0986ad54a1d5d5c6bbabda2f8d
silvercho <tjddms01212@gmail.com>
2023-11-03 18:09:37 +0900

    init

---
+++ src/test/java/com/example/mission_choseongeun/MissionChoSeongEunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mission_choseongeun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MissionChoSeongEunApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
