d57787eaee04d0c2309b61df6af55b42ab405952
silvercho <tjddms01212@gmail.com>
2023-11-06 13:56:38 +0900

    5단계 목록

--- src/main/java/com/example/mission_choseongeun/App.java
+++ src/main/java/com/example/mission_choseongeun/App.java
@@ -1,10 +1,13 @@
 package com.example.mission_choseongeun;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
     private final Scanner scanner;
     private int nextQuoteNumber =1; // 명언 번호 변수
+    private List<String> quotes = new ArrayList<>();
 
     public App(){
         scanner = new Scanner(System.in);
@@ -25,8 +28,18 @@ public class App {
                 System.out.print("작가 : ");
                 String authorName = scanner.nextLine();
 
+                String newQuote = nextQuoteNumber + " / " + authorName + " / " + content; //"번호 / 작가 / 명언"
+                quotes.add(newQuote);
+
                 System.out.println(nextQuoteNumber + "번 명언이 등록 되었습니다.");
                 nextQuoteNumber++; //번호는 추가될 때 마다  계속 증가한다.
+            } else if (cmd.equals("목록")){
+                System.out.println("번호 / 작가 / 명언");
+                System.out.println("----------------------");
+                for (String quote : quotes) {
+                    System.out.println(quote);
+                }
+
             }
         }
     }
