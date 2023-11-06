4fdee6c9fcf5288ef41c96bb56f40be8dd440d65
silvercho <tjddms01212@gmail.com>
2023-11-06 13:33:18 +0900

    2단계

--- src/main/java/com/example/mission_choseongeun/App.java
+++ src/main/java/com/example/mission_choseongeun/App.java
@@ -18,6 +18,11 @@ public class App {
 
             if (cmd.equals("종료")){
                 break;
+            } else if (cmd.equals("등록")){
+                System.out.println("명언 : ");
+                String content = scanner.nextLine();
+                System.out.println("작가 : ");
+                String authorName = scanner.nextLine();
             }
         }
     }
