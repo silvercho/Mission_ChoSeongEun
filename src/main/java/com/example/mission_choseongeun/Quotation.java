8aea0a77283fec8bd05fff8f494c445a8ba4c6bf
silvercho <tjddms01212@gmail.com>
2023-11-08 15:35:37 +0900

    9ã단계 +ãa 롬복,getter,setter생성자 추가

--- src/main/java/com/example/mission_choseongeun/Quotation.java
+++ src/main/java/com/example/mission_choseongeun/Quotation.java
@@ -1,37 +1,19 @@
 package com.example.mission_choseongeun;
 
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.io.Serializable;
 
+@Getter
+@Setter
 public class Quotation implements Serializable {
+
     private static final long serialVersionUID = 1L;
     int id;
     String content;
     String authorName;
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public void setAuthorName(String authorName) {
-        this.authorName = authorName;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public String getAuthorName() {
-        return authorName;
-    }
 
     public Quotation(int id, String content, String authorName) { //생성자 만들기 alt+insert
         this.id = id;
